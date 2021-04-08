dab2b235ad2b2786579d429bfa4b32c1f62997e2
rudyhostyn <68102881+rudyhostyn@users.noreply.github.com>
2021-04-08 08:49:16 +0200

    version 2

--- Funcion_sumatodos.py
+++ Funcion_sumatodos.py
@@ -1,9 +1,14 @@
 def sumatodos(limitTo):
     resultado = 0
-    for i in range (1,limitTo):
+    for i in range (1,limitTo+1):
         resultado += i
     return resultado
     
+def sumatodosloscuadrados(limitTo):
+    resultado = 0
+    for i in range (1, limitTo+1):
+        resultado += i*i
+    return resultado
 
 print(sumatodos(100))
-
+print(sumatodosloscuadrados(4))
